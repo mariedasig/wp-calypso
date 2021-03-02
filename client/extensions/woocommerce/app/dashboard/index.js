08cccf786d0becef7b9ba0931db1810106338723
Matt Sherman <matt@jam123.com>
2021-03-02 18:19:03 -0500

    Redirect to WooCommerce installation page from Store dashboard

--- client/extensions/woocommerce/app/dashboard/index.js
+++ client/extensions/woocommerce/app/dashboard/index.js
@@ -4,6 +4,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -12,14 +13,34 @@ import { localize } from 'i18n-calypso';
 import ActionHeader from 'woocommerce/components/action-header';
 import Main from 'calypso/components/main';
 import StoreMoveNoticeView from './store-move-notice-view';
+import { getSelectedSiteWithFallback } from 'woocommerce/state/sites/selectors';
+import { getSiteOption } from 'calypso/state/sites/selectors';
 
 class Dashboard extends Component {
 	static propTypes = {
 		className: PropTypes.string,
 	};
 
+	componentDidMount() {
+		this.maybeRedirectToWooCommerceInstallation( this.props );
+	}
+
+	shouldComponentUpdate( nextProps ) {
+		this.maybeRedirectToWooCommerceInstallation( nextProps );
+	}
+
+	maybeRedirectToWooCommerceInstallation( props ) {
+		if ( ! props.isSiteWpcomStore ) {
+			window.location = '/woocommerce-installation/' + props.siteSlug;
+		}
+	}
+
 	render() {
-		const { className, translate } = this.props;
+		const { className, isSiteWpcomStore, translate } = this.props;
+
+		if ( ! isSiteWpcomStore ) {
+			return null;
+		}
 
 		return (
 			<Main className={ classNames( 'dashboard', className ) } wideLayout={ true }>
@@ -30,4 +51,19 @@ class Dashboard extends Component {
 	}
 }
 
-export default localize( Dashboard );
+function mapStateToProps( state ) {
+	const selectedSite = getSelectedSiteWithFallback( state );
+	const siteId = selectedSite ? selectedSite.ID : null;
+	const siteSlug = selectedSite ? selectedSite.slug : '';
+
+	// Assume that we are dealing with a site that had Store until we can
+	// check for sure, to avoid prematurely redirecting away from the dashboard
+	const isSiteWpcomStore = siteId ? getSiteOption( state, siteId, 'is_wpcom_store' ) : true;
+
+	return {
+		isSiteWpcomStore,
+		siteSlug,
+	};
+}
+
+export default connect( mapStateToProps, {} )( localize( Dashboard ) );
