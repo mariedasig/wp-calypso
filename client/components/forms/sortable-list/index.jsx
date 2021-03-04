d1b8e850b0e3d141c5b42dcb024df7578f3f9013
Marin Atanasov <tyxla@abv.bg>
2021-03-04 15:52:38 +0200

    Framework: Refactor away from _.fromPairs()

--- client/components/forms/sortable-list/index.jsx
+++ client/components/forms/sortable-list/index.jsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { localize } from 'i18n-calypso';
-import { assign, findIndex, fromPairs, noop } from 'lodash';
+import { assign, findIndex, noop } from 'lodash';
 import classNames from 'classnames';
 import debugFactory from 'debug';
 import Gridicon from 'calypso/components/gridicon';
@@ -286,7 +286,7 @@ class SortableList extends React.Component {
 					'is-draggable': isDraggable,
 				} );
 
-				events = fromPairs(
+				events = Object.fromEntries(
 					events.map( function ( event ) {
 						return [ event, this[ event ].bind( null, index ) ];
 					}, this )
